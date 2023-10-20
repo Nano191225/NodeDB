2e7db78dd6f9f4077d08d4647506709f84d4fa4a
Nano191225 <93137582+Nano191225@users.noreply.github.com>
2023-10-20 23:54:11 +0900

    Create NodeDB.js

---
+++ NodeDB.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const fs_1 = __importDefault(require("fs"));
+module.exports = class Database extends Map {
+    constructor(name) {
+        super();
+        if (typeof name !== "string")
+            throw new TypeError("Database name must be a string");
+        this.name = name;
+        if (!fs_1.default.existsSync(`./db/${name}/`)) {
+            fs_1.default.mkdirSync(`./db/${name}/`);
+        }
+        this.load();
+    }
+    load() {
+        const files = fs_1.default.readdirSync(`./db/${this.name}/`);
+        files.forEach(file => {
+            const data = fs_1.default.readFileSync(`./db/${this.name}/${file}`, {
+                encoding: "utf8",
+            });
+            const key = file.replace(".json", "");
+            super.set(key, JSON.parse(data));
+        });
+    }
+    set(key, value) {
+        this.keyCheck(key);
+        super.set(key, value);
+        fs_1.default.writeFileSync(`./db/${this.name}/${key}.json`, JSON.stringify(value, null, 4));
+        return this;
+    }
+    delete(key) {
+        this.keyCheck(key);
+        const result = super.delete(key);
+        fs_1.default.rmSync(`./db/${this.name}/${key}.json`);
+        return result;
+    }
+    clear() {
+        super.clear();
+        fs_1.default.rmSync(`./db/${this.name}/`, { recursive: true });
+    }
+    keyCheck(key) {
+        if (typeof key !== "string")
+            throw new TypeError("Database key must be a string");
+        if (key.search(/[^a-z0-9_]/gi) !== -1)
+            throw new TypeError("Key must only contain alphanumeric characters and underscores");
+    }
+};
